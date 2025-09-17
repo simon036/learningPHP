47495095f27d5429904acadf7858b050916e5d52
Simon <ndungusimon564@gmail.com>
2025-09-17 15:16:20 +0300

    math functions

---
+++ mathFunctions/math.php
@@ -0,0 +1,34 @@
+// pi () fn 
+<?php
+    echo(pi());
+?>
+
+// min() fn
+<?php
+    echo(min(0, 150, 30, 20, -8, -200));
+?>
+
+// max() fn
+<?php
+    echo(max(0, 150, 30, 20, -8, -200));
+?>\
+
+// abs() fn
+<?php
+echo(abs(-6.7));
+?>
+
+//Sqrt() fn
+<?php
+    echo(sqrt(64));
+?>
+// round() fn
+<?php
+echo(round(0.60));
+echo(round(0.49));
+?>
+
+//rand() fn
+<?php
+echo(rand());
+?>
